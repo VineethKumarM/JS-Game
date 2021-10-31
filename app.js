22289c98da2f21eec0bea08f86afcae7987b4b6d
VineethKumarM <munigyala@gmail.com>
2021-11-01 02:26:28 +0530

    game added

---
+++ app.js
@@ -0,0 +1,67 @@
+let squris = document.querySelectorAll('.squirell');
+let score = document.querySelector('#score')
+function randHole() {
+	return Math.floor(Math.random()*7) ;
+}
+var points = 0;
+function addPoints() {
+	points++;
+}
+let nIntervId;
+let timeID;
+
+function starter() {
+	score.innerHTML = "Game playtime is 2 mins";
+	startGame();
+	timeID = setTimeout(() => {
+		stopGame();
+	}, 2*60*1000);
+}
+
+function startGame() {
+  if (!nIntervId) {
+	points = 0;
+	squris.forEach(div => {
+		div.classList.toggle('hide',true);
+	});
+	nIntervId = setInterval(function() {
+		showUp();
+
+	}, 3000);
+  }
+  else {
+	  alert('complete the present game');
+  }
+}
+
+function showUp() {
+
+	let i = randHole();
+	squris[i].classList.add("slideIn");
+	
+	squris[i].addEventListener("click", function () {
+		points++;
+	})
+	setTimeout(function () {
+		squris[i].classList.remove("slideIn");
+	}, 1000)
+}
+
+function stopGame() {
+	if(nIntervId) {
+		clearInterval(nIntervId);
+		// squris.forEach(div => {
+		// 	div.classList.toggle('hide',true);
+		// 	});
+		score.innerText = `You caught ${points} squirrels! Try again to catch more`;
+		nIntervId = null;
+		points = 0; 
+		clearTimeout(timeID);
+	} else {
+		alert('please start the game');
+	}
+	
+}
+
+document.getElementById("start").addEventListener("click", starter);
+document.getElementById("stop").addEventListener("click", stopGame);
