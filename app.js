9223cc0f609cb03db989ecfccccdcb99681f48ad
VineethKumarM <munigyala@gmail.com>
2021-11-01 22:39:57 +0530

    added timer

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 let squris = document.querySelectorAll('.squirell');
-let nets = document.querySelectorAll('.net')
-let score = document.querySelector('#score')
+// let nets = document.querySelectorAll('.net')
+let score = document.querySelector('#scoreCard')
+let scoreB = document.querySelector('#scoreCard').querySelector('p');
 function randHole() {
 	return Math.floor(Math.random()*9) ;
 }
 var points = 0;
 function addPoints(idx) {
-	document.getElementById(idx).classList.add('net-active');
+	// document.getElementById(idx).classList.add('net-active');
 	points++;
+	scoreB.innerText = `${points} catches`
+
 }
 let nIntervId;
 let timeID;
+const TIME_LIMIT = 2*60;
+let timePassed = 0;
+let timerInterval = null;
+let timeLeft = TIME_LIMIT;
+document.querySelector('#timeTag').innerHTML = `${formatTime(timeLeft)}`;
 
 function starter() {
-	score.innerHTML = "Game playtime is 2 mins";
 	startGame();
+	startTimer();
 	timeID = setTimeout(() => {
 		stopGame();
 	}, 2*60*1000);
@@ -23,16 +31,18 @@ function starter() {
 function startGame() {
   if (!nIntervId) {
 	points = 0;
+	score.classList.remove('hide')
+	scoreB.innerHTML = 'Game Started!'
 	squris.forEach(div => {
 		div.classList.toggle('hide',true);
 	});
-	nets.forEach(net => {
-		net.classList.toggle('hide',true)
-	});
+	// nets.forEach(net => {
+	// 	net.classList.toggle('hide',true)
+	// });
 	nIntervId = setInterval(function() {
 		showUp();
 
-	}, 2500);
+	}, 1800);
   }
   else {
 	  alert('complete the present game');
@@ -45,14 +55,18 @@ function showUp() {
 	squris[i].classList.add("slideIn");
 	setTimeout(function () {
 		squris[i].classList.remove("slideIn");
-		nets[i].classList.remove("net-active")
-	}, 1400)
+		// nets[i].classList.remove("net-active")
+	}, 1200)
 }
 
 function stopGame() {
 	if(nIntervId) {
 		clearInterval(nIntervId);
-		score.innerText = `You caught ${points} squirrels! Try again to catch more`;
+		clearInterval(timerInterval);
+		timeLeft = TIME_LIMIT;
+		timePassed = 0;
+		document.querySelector('#timeTag').innerHTML = "0:00";
+		scoreB.innerText = `Your points are ${points}`;
 		nIntervId = null;
 		points = 0; 
 		clearTimeout(timeID);
@@ -64,3 +78,23 @@ function stopGame() {
 
 document.getElementById("start").addEventListener("click", starter);
 document.getElementById("stop").addEventListener("click", stopGame);
+
+//timer 
+
+function formatTime(time) {
+	const minutes = Math.floor(time / 60);
+	let seconds = time % 60;
+	if (seconds < 10) {
+	  seconds = `0${seconds}`;
+	}
+	return `${minutes}:${seconds}`;
+  }
+
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("timeTag").innerHTML = formatTime(timeLeft);
+  }, 1000);
+}
